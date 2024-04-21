0c242d4d91450297dab99f358011d7aa299dd658
Ismo Yliviiri <iyliviir@edu.lapinamk.fi>
2024-04-21 15:17:12 +0300

    Sentiment analysis

--- app.py
+++ app.py
@@ -8,18 +8,13 @@ app = Flask(__name__)
 nltk.download('vader_lexicon')  # Download dictionary for sentiment analysis
 sia = SentimentIntensityAnalyzer()
 
-# Assign colors to the response
-positive_color = "#32CD32"  # green
-negative_color = "#FF6347"  # red
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         user_input = request.form["user_input"]
         sentiment_scores = sia.polarity_scores(user_input)
         sentiment = "Positive" if sentiment_scores["compound"] > 0 else "Negative"
-        color = positive_color if sentiment == "Positive" else negative_color
-        return render_template("result.html", user_input=user_input, sentiment=sentiment, color=color)
+        return render_template("results.html", user_input=user_input, sentiment=sentiment)
     return render_template("index.html")
 
 if __name__ == "__main__":

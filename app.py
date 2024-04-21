fab964e2fcb6a574971e60f0b3eb7c450e3feb70
Ismo Yliviiri <iyliviir@edu.lapinamk.fi>
2024-04-21 15:36:49 +0300

    send_file

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return send_file("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
